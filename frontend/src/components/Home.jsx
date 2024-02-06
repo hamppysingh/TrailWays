4b465335a08a982ce4e857b5f5f852f016059f7e
hamppysingh <praveenk012340@gmail.com>
2024-02-06 19:24:36 +0530

    project start 1.0

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+import { Carousel } from './Carousel'
+import "./Home.css";
+export const Home = () => {
+  return (
+    <>
+        <Navbar></Navbar>
+        <Carousel></Carousel>
+    </>
+  )
+}
